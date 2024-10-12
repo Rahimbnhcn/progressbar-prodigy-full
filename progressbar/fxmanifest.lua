46107832f69a8d22125bf3a991f7215dc8250175
echo <123136476+echodz@users.noreply.github.com>
2024-10-12 19:34:11 -0400

    Add files via upload

---
+++ progressbar/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+author 'ANTUNES'
+description 'ProgressBar System | Inspired on Prodigy 2.0'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_script 'client.lua'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
